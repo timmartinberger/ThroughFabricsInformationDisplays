cc62a2f605d9cdc0d6b4b6fc76a383ff7d180029
jhein <johann.hein.1997@gmail.com>
2022-07-15 16:17:45 +0200

    Bug Fixes

--- informationalDisplaysControl/app/src/main/java/de/uni_hannover/hci/informationalDisplaysControl/baseData/Symbol.java
+++ informationalDisplaysControl/app/src/main/java/de/uni_hannover/hci/informationalDisplaysControl/baseData/Symbol.java
@@ -16,4 +16,47 @@ public enum Symbol {
     CAT,
     CHERRY,
     LOCK;
+
+    public String[] getDrawableArray() {
+        return codeFromList() ;
+    }
+
+    private String[] lockCode = {"", ""};
+    private String[] codeFromList() {
+        switch(this) {
+            case MONEY:
+                break;
+            case HEART:
+                break;
+            case HOUSE:
+                break;
+            case CIRCLE:
+                break;
+            case SQUARE:
+                break;
+            case QUESTION_MARK:
+                break;
+            case BELL:
+                break;
+            case MUSIC:
+                break;
+            case TIME:
+                break;
+            case KEY:
+                break;
+            case DUCK:
+                break;
+            case GLASSES:
+                break;
+            case CAT:
+                break;
+            case CHERRY:
+                break;
+            case LOCK:
+                return lockCode;
+        }
+        return null;
+    }
 }
+
+
